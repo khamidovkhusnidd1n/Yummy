96ea8f9485a038011e5c922a79e604c3d80dff37
KhamidovKhusniddin <ki770760636@gmail.com>
2026-01-29 11:26:45 +0500

    Update menu from admin panel

--- menu_data.js
+++ menu_data.js
@@ -1,85 +1,274 @@
-// Ultimate Robust Menu Data
+
+// Auto-generated menu data
 window.DYNAMIC_MENU_DATA = {
     "🔥 Specials": [
-        { "n": "Kfc BURGER (+0.25 Cola)", "p": 33000, "i": "images/image2/kfc_burger.png" },
-        { "n": "Kfc HAGGI (+0.25 Cola)", "p": 38000, "i": "images/image2/kfc_haggi_cola.png" }
+        {
+            "n": "Kfc BURGER (+0.25 Cola)",
+            "p": 33000,
+            "i": "images/image2/kfc_burger.png"
+        },
+        {
+            "n": "Kfc HAGGI (+0.25 Cola)",
+            "p": 38000,
+            "i": "images/image2/kfc_haggi_cola.png"
+        }
     ],
     "🍔 MAZZA": [
-        { "n": "MAZZA (Kichik)", "p": 15000, "i": "images/mazza.png" },
-        { "n": "MAZZA (Katta)", "p": 30000, "i": "images/mazza.png" }
+        {
+            "n": "MAZZA (Kichik)",
+            "p": 15000,
+            "i": "images/mazza.png"
+        },
+        {
+            "n": "MAZZA (Katta)",
+            "p": 30000,
+            "i": "images/mazza.png"
+        }
     ],
     "🌯 Lavash": [
-        { "n": "Standart", "p": 30000, "i": "images/lavash.png" },
-        { "n": "Pishloqli", "p": 35000, "i": "images/image2/images3/lavash_cheese.png" },
-        { "n": "Big", "p": 40000, "i": "images/image2/images3/lavash_big.png" },
-        { "n": "Big pishloq", "p": 45000, "i": "images/image2/images3/lavash_cheese.png" }
+        {
+            "n": "Standart",
+            "p": 30000,
+            "i": "images/lavash.png"
+        },
+        {
+            "n": "Pishloqli",
+            "p": 35000,
+            "i": "images/image2/images3/lavash_cheese.png"
+        },
+        {
+            "n": "Big",
+            "p": 40000,
+            "i": "images/image2/images3/lavash_big.png"
+        },
+        {
+            "n": "Big pishloq",
+            "p": 45000,
+            "i": "images/image2/images3/lavash_cheese.png"
+        }
     ],
     "🍔 Burger": [
-        { "n": "Gamburger", "p": 28000, "i": "images/burger.png" },
-        { "n": "Chizburger", "p": 33000, "i": "images/image2/images3/cheeseburger.png" },
-        { "n": "Double", "p": 35000, "i": "images/image2/images3/double_burger.png" },
-        { "n": "Double chiz", "p": 40000, "i": "images/image2/images3/double_cheese.png" }
+        {
+            "n": "Gamburger",
+            "p": 28000,
+            "i": "images/burger.png"
+        },
+        {
+            "n": "Chizburger",
+            "p": 33000,
+            "i": "images/image2/images3/cheeseburger.png"
+        },
+        {
+            "n": "Double",
+            "p": 35000,
+            "i": "images/image2/images3/double_burger.png"
+        },
+        {
+            "n": "Double chiz",
+            "p": 40000,
+            "i": "images/image2/images3/double_cheese.png"
+        }
     ],
     "🥙 Doner": [
-        { "n": "Standart Doner", "p": 25000, "i": "images/image2/donar.png" },
-        { "n": "Katta Doner", "p": 30000, "i": "images/image2/images3/donerbig.png" },
-        { "n": "Pishloqli Doner", "p": 35000, "i": "images/image2/images3/donercheese.png" }
+        {
+            "n": "Standart Doner",
+            "p": 25000,
+            "i": "images/image2/donar.png"
+        },
+        {
+            "n": "Katta Doner",
+            "p": 30000,
+            "i": "images/image2/images3/donerbig.png"
+        },
+        {
+            "n": "Pishloqli Doner",
+            "p": 35000,
+            "i": "images/image2/images3/donercheese.png"
+        }
     ],
     "🍗 KFC": [
-        { "n": "File strips", "p": 90000, "i": "images/image2/images3/Fillet_strips.png" },
-        { "n": "Qanot", "p": 85000, "i": "images/image2/images3/wings.png" },
-        { "n": "Lunch box", "p": 30000, "i": "images/image2/images3/lunch_box.png" },
-        { "n": "Kfc xaggi", "p": 30000, "i": "images/image2/images3/KFC_xaggi.png" }
+        {
+            "n": "File strips",
+            "p": 90000,
+            "i": "images/image2/images3/Fillet_strips.png"
+        },
+        {
+            "n": "Qanot",
+            "p": 85000,
+            "i": "images/image2/images3/wings.png"
+        },
+        {
+            "n": "Lunch box",
+            "p": 30000,
+            "i": "images/image2/images3/lunch_box.png"
+        },
+        {
+            "n": "Kfc xaggi",
+            "p": 30000,
+            "i": "images/image2/images3/KFC_xaggi.png"
+        }
     ],
     "🌭 Xot-dog": [
-        { "n": "Mini", "p": 10000, "i": "images/image2/images3/hot_dog.png" },
-        { "n": "Twins", "p": 17000, "i": "images/image2/images3/twins.png" },
-        { "n": "Big (45sm)", "p": 22000, "i": "images/image2/images3/hotdogbig.png" },
-        { "n": "Super (45sm)", "p": 25000, "i": "images/image2/hot_dog.png" },
-        { "n": "Canada (45sm)", "p": 25000, "i": "images/hotdog_canada.png" }
+        {
+            "n": "Mini",
+            "p": 10000,
+            "i": "images/image2/images3/hot_dog.png"
+        },
+        {
+            "n": "Twins",
+            "p": 17000,
+            "i": "images/image2/images3/twins.png"
+        },
+        {
+            "n": "Big (45sm)",
+            "p": 22000,
+            "i": "images/image2/images3/hotdogbig.png"
+        },
+        {
+            "n": "Super (45sm)",
+            "p": 25000,
+            "i": "images/image2/hot_dog.png"
+        },
+        {
+            "n": "Canada (45sm)",
+            "p": 25000,
+            "i": "images/hotdog_canada.png"
+        }
     ],
     "🥪 Xaggi": [
-        { "n": "Standart", "p": 35000, "i": "images/image2/xaggi.png" },
-        { "n": "Big", "p": 45000, "i": "images/image2/images3/xaggi_big.png" },
-        { "n": "Xot let", "p": 28000, "i": "images/image2/xaggi.png" }
+        {
+            "n": "Standart",
+            "p": 35000,
+            "i": "images/image2/xaggi.png"
+        },
+        {
+            "n": "Big",
+            "p": 45000,
+            "i": "images/image2/images3/xaggi_big.png"
+        },
+        {
+            "n": "Xot let",
+            "p": 28000,
+            "i": "images/image2/xaggi.png"
+        }
     ],
     "🍗 Naggets": [
-        { "n": "Naggets", "p": 22000, "i": "images/naggets.png" },
-        { "n": "Naggets mini", "p": 11000, "i": "images/naggets.png" }
+        {
+            "n": "Naggets",
+            "p": 22000,
+            "i": "images/naggets.png"
+        },
+        {
+            "n": "Naggets mini",
+            "p": 11000,
+            "i": "images/naggets.png"
+        }
     ],
     "🧀 Pishloqli yostiqchalar": [
-        { "n": "Yostiqchalar", "p": 28000, "i": "images/pishloqli.png" },
-        { "n": "Yostiqchalar mini", "p": 14000, "i": "images/pishloqli.png" }
+        {
+            "n": "Yostiqchalar",
+            "p": 28000,
+            "i": "images/pishloqli.png"
+        },
+        {
+            "n": "Yostiqchalar mini",
+            "p": 14000,
+            "i": "images/pishloqli.png"
+        }
     ],
     "🥪 Klab sandwich": [
-        { "n": "Standart", "p": 35000, "i": "images/sandwich.png" }
+        {
+            "n": "Standart",
+            "p": 35000,
+            "i": "images/sandwich.png"
+        }
     ],
     "🍟 Fri": [
-        { "n": "Standart", "p": 15000, "i": "images/fri.png" },
-        { "n": "Big", "p": 17000, "i": "images/image2/images3/fribig.png" }
+        {
+            "n": "Standart",
+            "p": 15000,
+            "i": "images/fri.png"
+        },
+        {
+            "n": "Big",
+            "p": 17000,
+            "i": "images/image2/images3/fribig.png"
+        }
     ],
     "🥟 Somsa": [
-        { "n": "Go'shtli", "p": 10000, "i": "images/somsa.png" },
-        { "n": "Pishloqli tovuq", "p": 7000, "i": "images/image2/images3/chickensomsa.png" },
-        { "n": "Ko'k", "p": 5000, "i": "images/image2/images3/koksomsa.png" },
-        { "n": "Kartoshka", "p": 5000, "i": "images/image2/images3/kartoshkasomsa.png" }
+        {
+            "n": "Go'shtli",
+            "p": 10000,
+            "i": "images/somsa.png"
+        },
+        {
+            "n": "Pishloqli tovuq",
+            "p": 7000,
+            "i": "images/image2/images3/chickensomsa.png"
+        },
+        {
+            "n": "Ko'k",
+            "p": 5000,
+            "i": "images/image2/images3/koksomsa.png"
+        },
+        {
+            "n": "Kartoshka",
+            "p": 5000,
+            "i": "images/image2/images3/kartoshkasomsa.png"
+        }
     ],
     "☕️ Ichimliklar": [
-        { "n": "Choy", "p": 3000, "i": "images/image2/tea.png" },
-        { "n": "Kofe", "p": 5000, "i": "images/image2/coffee.png" },
-        { "n": "Kofe (Big)", "p": 7000, "i": "images/image2/coffee.png" },
-        { "n": "Choy mevali", "p": 10000, "i": "images/image2/tea_via_fruits.png" }
+        {
+            "n": "Choy",
+            "p": 3000,
+            "i": "images/image2/tea.png"
+        },
+        {
+            "n": "Kofe",
+            "p": 5000,
+            "i": "images/image2/coffee.png"
+        },
+        {
+            "n": "Kofe (Big)",
+            "p": 7000,
+            "i": "images/image2/coffee.png"
+        },
+        {
+            "n": "Choy mevali",
+            "p": 10000,
+            "i": "images/image2/tea_via_fruits.png"
+        },
+        {
+            "n": "kofe",
+            "p": 1200,
+            "i": "images/products/product_c1136d25.jpg"
+        }
     ],
     "🍹 Cocktails": [
-        { "n": "Tropic", "p": 15000, "i": "images/image2/tropic.png" },
-        { "n": "Moxito", "p": 15000, "i": "images/image2/moxito.png" },
-        { "n": "O'rmon mevalari", "p": 15000, "i": "images/image2/forest_fruits.png" }
+        {
+            "n": "Tropic",
+            "p": 15000,
+            "i": "images/image2/tropic.png"
+        },
+        {
+            "n": "Moxito",
+            "p": 15000,
+            "i": "images/image2/moxito.png"
+        },
+        {
+            "n": "O'rmon mevalari",
+            "p": 15000,
+            "i": "images/image2/forest_fruits.png"
+        }
     ],
     "🥫 Souslar": [
-        { "n": "Sous", "p": 4000, "i": "images/image2/sous.png" }
+        {
+            "n": "Sous",
+            "p": 4000,
+            "i": "images/image2/sous.png"
+        }
     ]
 };
-
 window.DYNAMIC_CATS = {
     "uz": {
         "🔥 Specials": "🔥 Specials",
